6931a874a23e6f84e06ef191f16614f28bccf06f
delio05 <91980225+delio05@users.noreply.github.com>
2023-10-18 12:19:42 -0500

    Add files via upload

---
+++ constant.lua
@@ -0,0 +1,7 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M={}
+M.bp=5
+return 5
